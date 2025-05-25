020ad07f18da36ce567155cf5e6c45d880bae71e
10Pratik01 <using0this@gmail.com>
2025-05-25 23:19:39 +0530

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
 const App = ()=>  {
   
 
   return (
     <>
-     
+     <Navbar/> 
+     <Hero/> 
+     <Highlights/> 
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import appleImg  from "../../public/assets/images/apple.svg"
+
+
+const Navbar = () => {
+
+    return(
+        <header>
+            <nav>
+                <img src={appleImg} alt=""/>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
