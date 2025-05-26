3a200007fb60e4d6102a3fe885ea3b67f2411305
10Pratik01 <using0this@gmail.com>
2025-05-26 23:17:43 +0530

    completed navbar and fix in index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
-import appleImg  from "../../public/assets/images/apple.svg"
-
+import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
+  return (
+    <header className="flex justify-center">
+      <nav className="flex justify-around w-full screem-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
 
-    return(
-        <header>
-            <nav>
-                <img src={appleImg} alt="Apple" width={14} height={18}/> 
+        <div className="flex gap-x-5 ">
+          {navLists.map((nav) => (
+            <div key={nav} className="cursor-pointer text-gray-500 hover:text-white transition-all">
+              {nav}
+            </div>
+          ))}
+        </div>
 
-                <div>
-                    {['Phones', 'Macbooks', 'Tablets'].map((nav)=> (
-                        <div key={nav}>
-                            {nav}
-                        </div>
-                    ))}
-                </div>
-
-
-            </nav>
-        </header>
-    )
+        <div className="flex gap-7 cursor-pointer">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Navbar
